66e020b0d8fa0830d1991064b4a34f82a4aa4b10
Stuart Harris <stuartaharris@gmail.com>
2017-06-04 23:37:27 -0500

    first commit

---
+++ config_example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //super easy setup, enter your twitter api credentials below and rename this file to config.js
+  consumer_key: 'ENTER YOUR DATA HERE',
+  consumer_secret: 'ENTER YOUR DATA HERE',
+  access_token_key: 'ENTER YOUR DATA HERE',
+  access_token_secret: 'ENTER YOUR DATA HERE'
+};
